166358c64bfeacfb886a8f20c57ebf75616603f5
hlphlphlp <hlp403@foxmail.com>
2020-03-01 11:48:01 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
-# @Author : Huang Liping
-# @Email : liping.a.huang@ericsson.com
+# @Author : HLP
 # @File : __init__.py.py
 # @Date : 2020/2/8 
 # @Desc : 
