6837cf1101873fbbd3c1325dd5fce55928aa36a3
ehualip <liping.a.huang@ericsson.com>
2021-01-18 20:18:16 +0800

    fix bug

--- index_for_fund_auto_notification.py
+++ index_for_fund_auto_notification.py
@@ -157,7 +157,7 @@ def get_result_fund_lst(fund_data, sort_key='assessment'):
         fund_increase_dic = change_fund_increase_dic(data_dic['code_id'], data_dic['name'])
         if data_dic['worth_to_buy'] and fund_increase_dic['assessment_worth'] < data_dic['worth_to_buy']:
             fund_increase_dic['fund_name'] = '【买买买】' + fund_increase_dic['fund_name']
-            excute_sql("update fund_info set worth_to_buy=%s where code_id='%s';" % (str(fund_increase_dic['assessment_worth'])),  fund_increase_dic['code_id'])
+            excute_sql("""update fund_info set worth_to_buy=%s where code_id='%s';""" % (str(fund_increase_dic['assessment_worth']),  fund_increase_dic['code_id']))
         if data_dic['worth_to_sell'] and fund_increase_dic['assessment_worth'] > data_dic['worth_to_sell']:
             fund_increase_dic['fund_name'] = '【卖卖卖】' + fund_increase_dic['fund_name']
 
